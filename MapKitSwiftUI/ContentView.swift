26530b5f755a570d49819121744fff030aa209b0
EnriqueDev <43639512+epoyatodev@users.noreply.github.com>
2023-09-17 19:03:43 +0200

    Initial Commit

---
+++ MapKitSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MapKitSwiftUI
+//
+//  Created by Enrique Poyato Ortiz on 17/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapKitSwiftUI/MapKitSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitSwiftUIApp.swift
+//  MapKitSwiftUI
+//
+//  Created by Enrique Poyato Ortiz on 17/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MapKitSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
